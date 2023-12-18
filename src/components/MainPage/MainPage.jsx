45fd7479b48ae66423d5116b4a7e12074dd94cb1
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-18 16:56:58 +0900

    feat: categorylist 완성

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
-
+import CategoryList from './CategoryList';
 
 const MainPage = () => {
     return (
-        <div>
-            <Banner></Banner>
-        </div>
+        <>
+            <Banner />
+            <CategoryList />
+        </>
     );
 };
 
