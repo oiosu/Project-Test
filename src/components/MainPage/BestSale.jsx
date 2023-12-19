3ceef882c597cf1f13d6ce43aed02dcb4478b63f
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-19 10:09:26 +0900

    오늘의 베스트 상품 페이지 완성

--- src/components/MainPage/BestSale.jsx
+++ src/components/MainPage/BestSale.jsx
@@ -4,49 +4,58 @@ import styled from 'styled-components';
 const BestSale = () => {
     return (
         <SaleSection>
-            <header>
+            <header className='new-title'>
                 <h1>오늘의 베스트 상품</h1>
-                <span>더보기</span>
+                {/* <p>더보기</p> */}
             </header>
             <div className='bigsale-item'>
                 <ul>
-                    {/* 01 */}
                     <li className='item-img'>
                         <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/168731059927618146.jpg?gif=1&w=360&h=360&c=c&q=0.8&webp=1' alt='01' />
                         <div className='product-img-text'>
-                            <spans className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</spans>
+                            <p className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</p>
                             <span className='item-rate'>63%</span>
                             <span className='item-price'>28,900</span>
                         </div>
+                        <div className='item-btn'>
+                            <button className='free-btn'>무료 배송</button>
+                            <button className='big-btn'>특가</button>
+                        </div>
                     </li>
-
-                    {/* 02 */}
                     <li className='item-img'>
                         <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/168731059927618146.jpg?gif=1&w=360&h=360&c=c&q=0.8&webp=1' alt='01' />
                         <div className='product-img-text'>
-                            <spans className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</spans>
+                            <p className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</p>
                             <span className='item-rate'>63%</span>
                             <span className='item-price'>28,900</span>
                         </div>
+                        <div className='item-btn'>
+                            <button className='free-btn'>무료 배송</button>
+                            <button className='big-btn'>특가</button>
+                        </div>
                     </li>
-
-                    {/* 03 */}
                     <li className='item-img'>
                         <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/168731059927618146.jpg?gif=1&w=360&h=360&c=c&q=0.8&webp=1' alt='01' />
                         <div className='product-img-text'>
-                            <spans className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</spans>
+                            <p className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</p>
                             <span className='item-rate'>63%</span>
                             <span className='item-price'>28,900</span>
                         </div>
+                        <div className='item-btn'>
+                            <button className='free-btn'>무료 배송</button>
+                            <button className='big-btn'>특가</button>
+                        </div>
                     </li>
-
-                    {/* 04 */}
                     <li className='item-img'>
                         <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/168731059927618146.jpg?gif=1&w=360&h=360&c=c&q=0.8&webp=1' alt='01' />
                         <div className='product-img-text'>
-                            <spans className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</spans>
-                            <p className='item-rate'>63%</p>
+                            <p className='item-title'>후기6만돌파! 40수 코마사 호텔수건 200g 10장</p>
+                            <span className='item-rate'>63%</span>
                             <span className='item-price'>28,900</span>
+                        </div>
+                        <div className='item-btn'>
+                            <button className='free-btn'>무료 배송</button>
+                            <button className='big-btn'>특가</button>
                         </div>
                     </li>
                 </ul>
@@ -60,64 +69,79 @@ export default BestSale;
 
 const SaleSection = styled.div`
 
-header{
+.new-title {
     display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    margin-left: 130px;
+    margin-right: 120px;
+    margin-top: 90px;
+}
+
+
+
+.bigsale-item {
+    display: flex;
+    flex-flow: row nowrap;
     justify-content: center;
-    margin-top: 70px;
 }
-h1 {
-    margin-right: 1400px;
-}
-span {
-    margin-top: 30px;
-    font-size: 16px;
-}
+
 .item-img {
     margin-right: 40px;
-    margin-left: 50px;
-}
-
-.bigsale-item {
-    float: left;
-}
-
-ul {
-    list-style:none;
-}
-
-li {
-    font-size: 18px;
-    float: left;
-}
-
-.bigsale-item {
-    margin-top: 20px;
-
+    margin-left: 40px;
 }
 
 img {
-    border-radius: 20px;
+    border-radius: 10px;
 }
 
-.product-img-text {
-    display: flex;
-    justify-content: center;
+ul {
+    list-style: none;
 }
-.item-title {
-    margin-top: 5px;
-    font-size: 13px;
-    font-weight: 400;
-    line-height: 17px;
-    max-height: 29px;
+
+li {
+    float: left;
 }
-.item-rate {
+
+.item-rate{
+    font-size: 20px;
     color: #6EB636;
-    font-size: 17px;
-    margin-right: 10px;
-    margin-left: 0px;
+    font-weight: bold;
+    float: left;
+    margin-left: 15px;
 }
+
 .item-price {
     font-weight: bold;
+    margin-right: 150px;
+    font-size: 20px;
+}
+
+.item-btn {
+    float: left;
+    margin-left: 10px;
+    margin-top: 10px;
+}
+
+.free-btn {
+    margin-right: 20px;
+    background-color: #D9D9D9;
+    border-radius: 10px;
+    padding: 8px;
+    color: #413F3F;
+    font-weight: bold;
+    font-size: 16px;
+    border: none;
+}
+
+.big-btn {
+    background-color: #FF9E9E;
+    border-radius: 10px;
+    padding: 8px;
+    color: #413F3F;
+    font-weight: bold;
+    font-size: 16px;
+    border: none;
 }
 
 `;
